c064cd31473176aef5722f2f708ef970dfa60ec6
samkessaram <samkessaram@gmail.com>
2015-11-19 17:15:31 +0000

    hide full review, reveal on click

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,39 +1,38 @@
 $(function(){
+// Configure/customize these variables.
+    var showChar = 375;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = '<i class="fa fa-angle-double-down show-text" title="View entire review"></i>';
+    var lesstext = '<i class="fa fa-angle-double-up show-text" title="Hide review"></i>';
+    
 
-  // var yelp = "http://api.yelp.com/";
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 
-  // $("#search").on("submit", function(event){
-  //   event.preventDefault();
-  //   var timestamp = Date.now() / 1000 | 0;
-  //   var location = $("#location").serialize();
-  //   var restaurant = $("#term").serialize();
-  //   var nonce = "";
-  //   var nonce_generator = function() {
-  //     nonce = "";
-  //     var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-  //     for ( i = 0 ; i < 33 ; i++) {
-  //       nonce = nonce + characters[Math.floor(Math.random()*characters.length)];
-  //     };
-  //   };
-  //   nonce_generator();
-  //   var promise = $.ajax({
-  //       url: yelp + "v2/search?" + restaurant + "&" + location,
-  //       jsonp:"$jsonp",
-  //       datatype:"$jsonp",
-  //       headers: {
-  //         Authorization:{
-  //           oauth_consumer_key:'Z0MtEf2s45VhWBxv7nR6-A',
-  //           oauth_token:'P47c2BKw82S_-bF-RzqpY4moxzDF26FC',
-  //           oauth_signature_method:"hmac-sha1",
-  //           oauth_signature:'3y7xIZ74bO0-PdiKXj9UVp3uD7I',
-  //           oauth_timestamp:timestamp,
-  //           oauth_nonce:nonce
-  //         }
-  //       }
-  //     });
-
-  //   console.log(promise);
-
-  // });
 
 });
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -32,10 +32,10 @@ class RestaurantsController < ApplicationController
 
     yelp = HTTParty.get(yelp_url, :headers=> headers)
     yelp_page = Nokogiri::HTML(yelp)
-    yelp_reviews = yelp_page.css('div.review-content p').to_s.split('</p>')[0..2].map { |review| review.split("ang=\"en\">")[1] }
+    @yelp_reviews = yelp_page.css('div.review-content p').to_s.split('</p>')[0..2].map { |review| review.split("ang=\"en\">")[1] }
     # binding.pry
-    @yelp_previews = yelp_reviews.map { |review| review[0..140] }
-    @yelp_fulltext = yelp_reviews.map { |review| review[141..review.length-1] }
+    # @yelp_previews = yelp_reviews.map { |review| review[0..140] }
+    # @yelp_fulltext = yelp_reviews.map { |review| review[141..review.length-1] }
 
     @yelp_ratings = yelp_page.css('div.review--with-sidebar div.review-content i.star-img').to_s.split("title=\"")[1..3].map { |rating| rating.split(" star rating")[0] }
     @yelp_dates = yelp_page.css('div span.rating-qualifier').to_s.split("datePublished\" content=\"")[1..3].map { |date| date[0..9]}
@@ -50,14 +50,14 @@ class RestaurantsController < ApplicationController
     @zomato_dates = date = zomato_page.xpath("//time").to_s.split("datetime=\"")[1..3].map { |date| date[0..9]}
 
     zomato_reviews_dirty = zomato_content.text.split("                                            ")[1..5]
-    zomato_reviews = []
+    @zomato_reviews = []
     zomato_reviews_dirty.each do |review|
       if (!review.include? "                    Rated") && (!review.include?("                \n                "))
-        zomato_reviews << review
+        @zomato_reviews << review
       end
     end
-    @zomato_previews = zomato_reviews.map { |review| review[0..140] }
-    @zomato_fulltext = zomato_reviews.map { |review| review[141..review.length-1] }
+    # @zomato_previews = zomato_reviews.map { |review| review[0..140] }
+    # @zomato_fulltext = zomato_reviews.map { |review| review[141..review.length-1] }
 
     # binding.pry
     search_name = @zomato_restaurant["name"].downcase
@@ -75,9 +75,9 @@ class RestaurantsController < ApplicationController
     open_table = HTTParty.get(open_table_url, :headers=> headers)
     open_table_page = Nokogiri::HTML(open_table)
     if !open_table_page.text.include? "We're sorry, but the page you requested could not be found."
-      open_table_reviews = open_table_page.css('#reviews-page p').to_s.split('</p>')[0..2]
-      @open_table_previews = open_table_reviews.map { |review| review[0..140] }
-      @open_table_fulltext = open_table_reviews.map { |review| review[141..review.length-1] }
+      @open_table_reviews = open_table_page.css('#reviews-page p').to_s.split('</p>')[0..2]
+      # @open_table_previews = open_table_reviews.map { |review| review[0..140] }
+      # @open_table_fulltext = open_table_reviews.map { |review| review[141..review.length-1] }
       @open_table_ratings = open_table_page.css('#reviews-results div.all-stars').to_s.split("title=\"")[1..3].map {|s| s[0].split("\" class")[0]}
       @open_table_dates = open_table_page.css('#reviews-results div.review-meta > span').to_s.split("color-light\">")[1..3].map { |date| date.split("<")[0] }
     end
@@ -87,9 +87,9 @@ class RestaurantsController < ApplicationController
     bookenda_page = Nokogiri::HTML(bookenda)
         # binding.pry
     if !bookenda_page.text.include? ("We're sorry, but the page you requested could not be found.")
-      bookenda_reviews = bookenda_page.css('div#containerReview div.row p').to_s.split("itemprop=\"description\">")[1..3].map { |review| review.split("</p>")[0] }
-      @bookenda_previews = bookenda_reviews.map { |review| review[0..140] }
-      @bookenda_fulltext = bookenda_reviews.map { |review| review[141..review.length-1] }
+      @bookenda_reviews = bookenda_page.css('div#containerReview div.row p').to_s.split("itemprop=\"description\">")[1..3].map { |review| review.split("</p>")[0] }
+      # @bookenda_previews = bookenda_reviews.map { |review| review[0..140] }
+      # @bookenda_fulltext = bookenda_reviews.map { |review| review[141..review.length-1] }
       @bookenda_ratings = bookenda_page.css('div#containerReview div.row div.score meta').to_s.split("itemprop=\"ratingValue\" content=\"")[1..3].map { |rating| rating[0..3] }
       @bookenda_dates = bookenda_page.css('div#containerReview div.row small').to_s.split("content=\"")[1..3].map { |date| date[0..9] }
     end
