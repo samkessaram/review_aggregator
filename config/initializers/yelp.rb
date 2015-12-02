4b97cc69e64c6c5b1212604ff398697ff89e9932
samkessaram <samkessaram@gmail.com>
2015-12-02 21:18:19 +0000

    Revert "changed keys"
    This reverts commit c89b628eabb5da0341bd2573c2413e8a814ab643.

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,9 +1,14 @@
+# client = Yelp::Client.new({ consumer_key: "Z0MtEf2s45VhWBxv7nR6-A",
+#                                 consumer_secret: "NH5hl8yEwduDzZr9dgBZvEeQUtY",
+#                                 token: "P47c2BKw82S_-bF-RzqpY4moxzDF26FC",
+#                                 token_secret: "3y7xIZ74bO0-PdiKXj9UVp3uD7I"
+#                               })
+
 require 'yelp'
 
 Yelp.client.configure do |config|
-  config.consumer_key = @@YELP_KEY
-  config.consumer_secret = @@YELP_SECRET
-  config.token = @@YELP_TOKEN
-  config.token_secret = @@YELP_TOKEN_SECRET
+  config.consumer_key = "Z0MtEf2s45VhWBxv7nR6-A"
+  config.consumer_secret = "NH5hl8yEwduDzZr9dgBZvEeQUtY"
+  config.token = "P47c2BKw82S_-bF-RzqpY4moxzDF26FC"
+  config.token_secret = "3y7xIZ74bO0-PdiKXj9UVp3uD7I"
 end
-
