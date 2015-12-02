d4e30116ada7bebd1c3973a5e1db19517b9c9395
samkessaram <samkessaram@gmail.com>
2015-12-02 21:21:25 +0000

    check ignore

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-@@YELP_KEY = 'XOm45rf5AhxBymCVnugG6Q'
-@@YELP_SECRET = 'BL9wgl9nEhBUDhtBPiJnaUZNx-M'
-@@YELP_TOKEN = 'QCyWbM0ZCBvZIsHwTtkY6XHO6PUbBBaG'
-@@YELP_TOKEN_SECRET = '42aLEGJxc9mjkl6V58iSy-ZI3os'
-
-@@ZOMATO_KEY = '5cbf55c44f90b88c427aeb18f437721c'
-
-Time.zone = "America/New_York"
+TEST MOTHER FUCKER
 
