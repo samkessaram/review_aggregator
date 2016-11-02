ccd0a7a0a1ee22a4403d2904ff53d88118e23270
samkessaram <samkessaram@gmail.com>
2016-11-02 17:47:03 +0000

    fixed date scrape and format for yelp reviews

--- app/services/reviews_finder.rb
+++ app/services/reviews_finder.rb
@@ -21,7 +21,7 @@ class ReviewsFinder
     @y_parsed = Nokogiri::HTML(y_raw)
     y_reviews = @y_parsed.css('div.review-content p').to_s.split('</p>')[0..2].map { |review| review.split("ang=\"en\">")[1] }
     y_ratings = @y_parsed.css('div.review--with-sidebar div.review-content i.star-img').to_s.split("title=\"")[1..3].map { |rating| rating.split(" star rating")[0].split('.0')[0] }
-    @y_dates = @y_parsed.css('div span.rating-qualifier').to_s.split("qualifier\">\n")[2..4].map { |date| Chronic.parse(date[8..17]).strftime('%b %d, %Y')}
+    @y_dates = @y_parsed.css('div.review-content span.rating-qualifier').to_s.split("qualifier\">\n")[1..3].map { |date| Chronic.parse(date[8..20]).strftime('%b %d, %Y')}
 
     {dates: @y_dates, reviews: y_reviews, ratings: y_ratings, url: y_url}
   end
