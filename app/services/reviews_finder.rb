756a530c72494e0479a95d498cf1c824ff94ef53
samkessaram <samkessaram@gmail.com>
2017-01-04 05:01:37 +0000

    fixed syntax error

--- app/services/reviews_finder.rb
+++ app/services/reviews_finder.rb
@@ -6,8 +6,8 @@ class ReviewsFinder
     
     data = {
       :reviews => {
-        :yelp => scrape_yelp,
-        # :yelp => {dates: [1,2,3], reviews: [1,2,3], ratings: [1,2,3], url: 'y_url'}
+        # :yelp => scrape_yelp,
+        :yelp => {dates: [1,2,3], reviews: [1,2,3], ratings: [1,2,3], url: 'y_url'},
         :zomato => scrape_zomato,
         :opentable => scrape_opentable,
         :bookenda => scrape_bookenda
